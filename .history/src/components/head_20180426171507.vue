38de6ec82afbedebdc9f423971f65d71bc2af5dc
Ivyfcy <787420277@qq.com>
2018-06-28 16:36:15 +0800

    vue-cli

---
+++ .history/src/components/head_20180426171507.vue
@@ -0,0 +1,8 @@
+<template>
+    <header>
+        <span class="back" @click="goBack">
+            <i class="icon icon-back"></i>
+            返回
+        </span>
+    </header>
+</template>
---
+++ .history/src/components/head_20180426171610.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <span class="back" @click="goBack">
+            <i class="icon icon-back"></i>
+            返回
+        </span>
+    </header>
+</template>
+<script>
+export default {
+  methods: {
+    goBack () {
+      console.log(2222)
+    }
+  }
+}
+</script>
+<style>
+
+</style>
