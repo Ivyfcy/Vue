6983b1dc1bbf04e68f266a6b57faf42d119bf524
Ivyfcy <787420277@qq.com>
2018-08-09 18:37:00 +0800

    添加轮播图

---
+++ src/pages/home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <span class="back"  @click="goBack">
+            <i class="icon icon-back"></i>
+            首页 {{my.pageSize}} 
+        </span>
+        <hello-world></hello-world>
+        <home-swiper></home-swiper>
+    </div>
+</template>
+<script>
+import helloWorld from '../components/HelloWorld'
+import HomeSwiper from '../components/swiper'
+import Axios from 'axios'
+export default {
+    data() {
+        return {
+            my: {}
+        }
+    },
+    mounted() {
+        const self = this
+        Axios.get('/api/posts').then(data => {
+            self.my = data.data
+        })
+    },
+    methods: {
+        goBack() {
+            console.log('点击首页')
+        }
+    },
+    components: {
+        helloWorld,
+        HomeSwiper
+    }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983
+}
+</style>
