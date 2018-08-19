d8df77a0cd4dbc8c08e9e611683b40c72858137a
mike <extrnight@126.com>
2018-08-19 08:30:26 +0800

    父子传值

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,19 +3,31 @@
         <span class="back"  @click="goBack">
             <i class="icon icon-back"></i>
             <home-swiper></home-swiper>
-            首页 {{my.pageSize}} 
+                首页 {{my.pageSize}} 
         </span>
-        <hello-world></hello-world>
+        <input type="text" v-model="todoValue">
+        <button @click="hand">提交</button>
+        <hello-world 
+            v-for="(item,index) in list" 
+            v-bind:content='item'
+            v-bind:key="index"
+            v-bind:index="index"
+            @delete="handledetele">
+         </hello-world>
     </div>
 </template>
 <script>
+// 父组件通过 v-bind:content（:content）向子组件传值，子组件得从props接收
+// 子组件通过$emit()事件向上一层触发事件，父组件在监听
 import helloWorld from '../components/HelloWorld'
 import HomeSwiper from '../components/swiper'
 import Axios from 'axios'
 export default {
     data() {
         return {
-            my: {}
+            my: {},
+            list: [],
+            todoValue: ''
         }
     },
     mounted() {
@@ -27,6 +39,13 @@ export default {
     methods: {
         goBack() {
             console.log('点击首页')
+        },
+        hand() {
+            this.list.push(this.todoValue)
+            this.todoValue = ''
+        },
+        handledetele(index) {
+            this.list.splice(index, 1)
         }
     },
     components: {
